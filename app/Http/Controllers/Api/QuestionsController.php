9c19992a052c8c158959175abe0b138effaf1c70
Daniel <stojeckidaniel@gmail.com>
2021-01-11 18:32:55 +0100

    fix conflict

--- app/Http/Controllers/Api/QuestionsController.php
+++ app/Http/Controllers/Api/QuestionsController.php
@@ -54,7 +54,7 @@ class QuestionsController extends Controller
 
         if($check) {
             $msg = 'Odpowiedzi poprawne, jestes autoryzowanym buggerem.';
-            session()->push('is_authorized', true);
+            session()->put('is_authorized', true);
         }
 
         return response()->json([
